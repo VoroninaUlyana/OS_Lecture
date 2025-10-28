1d995206ad1544b6336cf9b07e51a8e105dbce7a
Ulyana Voronina <ulyana.s.v.1@gmail.com>
2025-10-28 22:51:17 +0300

    Fibonaccci calculation function

--- OS_Lecture1/fib.cpp
+++ OS_Lecture1/fib.cpp
@@ -1,6 +1,24 @@
 #include "fib.h"
+#include <stdexcept>
 using namespace std;
 vector<unsigned long long> fib(unsigned int n) 
 {
-    return {};
+    if (n == 0)
+    {
+        throw invalid_argument("n must be > 0");
+    }
+    vector<unsigned long long> fib;
+    fib.reserve(n);
+    fib.push_back(1);
+    if (n == 1)
+    {
+        return fib;
+    }
+    fib.push_back(1);
+    for (unsigned int i = 2; i < n; ++i) 
+    {
+        unsigned long long next = fib[i - 1] + fib[i - 2];
+        fib.push_back(next);
+    }
+    return fib;
 }

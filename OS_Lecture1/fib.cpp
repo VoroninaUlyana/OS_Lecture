a1042da2cec589e233f72adca56a09c0ca71217a
Ulyana Voronina <ulyana.s.v.1@gmail.com>
2025-10-28 21:52:17 +0300

    creating the basis of the program

---
+++ OS_Lecture1/fib.cpp
@@ -0,0 +1,6 @@
+#include "fib.h"
+using namespace std;
+vector<unsigned long long> fib(unsigned int n) 
+{
+    return {};
+}
---
+++ OS_Lecture1/fib.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <vector>
+using namespace std;
+vector<unsigned long long> Fib(unsigned int n);

bc44a61580b38856099afb5c71df375edabb8976
Ulyana Voronina <ulyana.s.v.1@gmail.com>
2025-10-28 23:35:56 +0300

    tests

--- OS_Lecture1/OS_Lecture1.cpp
+++ OS_Lecture1/OS_Lecture1.cpp
@@ -1,6 +1,25 @@
 ﻿#include <iostream>
+#include <cassert>
 #include "fib.h"
 using namespace std;
+void runTests()
+{
+    {
+        auto v = Fib(1);
+        assert(v.size() == 1 && v[0] == 1);
+    }
+    {
+        auto v = Fib(2);
+        assert(v.size() == 2 && v[0] == 1 && v[1] == 1);
+    }
+    {
+        auto v = Fib(6);
+        unsigned long long expected[] = { 1, 1, 2, 3, 5, 8 };
+        for (int i = 0; i < 6; ++i)
+            assert(v[i] == expected[i]);
+    }
+    cout << "All tests passed successfully!\n";
+}
 int main() 
 {
     try {
@@ -19,5 +38,6 @@ int main()
     {
         cerr << "Error: " << e.what() << '\n';
     }
+    runTests();
     return 0;
 }

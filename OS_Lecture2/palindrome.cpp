c66d3444bb9a99a125fac53ca9dcc9677831a867
Ulyana Voronina <ulyana.s.v.1@gmail.com>
2025-10-29 01:07:09 +0300

    palindrome logic

--- OS_Lecture2/palindrome.cpp
+++ OS_Lecture2/palindrome.cpp
@@ -1,5 +1,11 @@
 #include "palindrome.h"
+#include <string>
+#include <algorithm>
+using namespace std;
 bool palindrome(int number)
 {
-    return true;
+    string s = to_string(number);
+    string reversed = s;
+    reverse(reversed.begin(), reversed.end());
+    return s == reversed;
 }

4d5b01833929fb3e11850628eb457bd017249643
Ulyana Voronina <ulyana.s.v.1@gmail.com>
2025-10-29 16:12:58 +0300

    tests

--- OS_Lecture3/OS_Lecture3.cpp
+++ OS_Lecture3/OS_Lecture3.cpp
@@ -1,8 +1,45 @@
 ﻿#include "list.h"
 #include <iostream>
+#include <cassert>
 using namespace std;
+void runTests()
+{
+    {
+        Node* head = nullptr;
+        newelem(head, 49);
+        newelem(head, 33);
+        newelem(head, 81);
+        assert(head != nullptr);
+        assert(head->data == 49);
+        assert(head->ssylka->data == 33);
+        assert(head->ssylka->ssylka->data == 81);
+        clearlist(head);
+        assert(head == nullptr);
+    }
+    {
+        Node* head = nullptr;
+        newelem(head, 49);
+        newelem(head, 29);
+        newelem(head, 1);
+        head = reverselist(head);
+        assert(head->data == 1);
+        assert(head->ssylka->data == 29);
+        assert(head->ssylka->ssylka->data == 49);
+        clearlist(head);
+        assert(head == nullptr);
+    }
+    {
+        Node* head = nullptr;
+        for (int i = 0; i < 5; ++i)
+            newelem(head, i);
+        clearlist(head);
+        assert(head == nullptr);
+    }
+    cout << "\nAll tests passed successfully!\n\n";
+}
 int main() 
 {
+    runTests();
     srand(static_cast<unsigned int>(time(nullptr)));
     int choice, n;
     Node* head = nullptr;
